77cb0d4587451f93cfd307521d16cf310655c9ed
omkar singh <singhomkar20.1995@gmail.com>
2024-08-11 15:23:15 +0530

    model trainer added

--- app.py
+++ app.py
@@ -3,10 +3,11 @@ from signLanguage.pipeline.training_pipeline import TrainPipeline
 from signLanguage.exception import SignException
 
 
-def run_pipeline() -> None:
+def run_pipeline() -> str:
     try: 
         obj = TrainPipeline()
         obj.run_pipeline()
+        return "Training succesful thanks for being here"
     except Exception as e:
         raise SignException(e, sys)
 
--- signLanguage/pipeline/training_pipeline.py
+++ signLanguage/pipeline/training_pipeline.py
@@ -2,20 +2,23 @@ import sys, os
 from signLanguage.logger import logging
 from signLanguage.exception import SignException
 from signLanguage.components.data_ingestion import DataIngestion
-
-
-from signLanguage.entity.config_entity import (DataIngestionConfig)
+from signLanguage.components.data_validation import DataValidation
+from signLanguage.entity.config_entity import (DataIngestionConfig,DataValidationConfig,ModelTrainerConfig)
                            
+from signLanguage.components.model_trainer import ModelTrainer
 
 
-from signLanguage.entity.artifacts_entity import (DataIngestionArtifact)
+from signLanguage.entity.artifacts_entity import (DataIngestionArtifact,DataValidationArtifact,ModelTrainerArtifact)
                            
 
 
 class TrainPipeline:
     def __init__(self):
         self.data_ingestion_config = DataIngestionConfig()
-   
+        self.data_validation_config = DataValidationConfig()
+
+        self.model_trainer_config = ModelTrainerConfig()
+
 
     
 
@@ -45,5 +48,42 @@ class TrainPipeline:
         try:
             data_ingestion_artifact = self.start_data_ingestion()
             
+        except Exception as e:
+            raise SignException(e, sys)
+        
+    def start_data_validation(
+        self, data_ingestion_artifact: DataIngestionArtifact
+    ) -> DataValidationArtifact:
+        logging.info("Entered the start_data_validation method of TrainPipeline class")
+
+        try:
+            data_validation = DataValidation(
+                data_ingestion_artifact=data_ingestion_artifact,
+                data_validation_config=self.data_validation_config,
+            )
+
+            data_validation_artifact = data_validation.initiate_data_validation()
+
+            logging.info("Performed the data validation operation")
+
+            logging.info(
+                "Exited the start_data_validation method of TrainPipeline class"
+            )
+
+            return data_validation_artifact
+
+        except Exception as e:
+            raise SignException(e, sys) from e
+        
+
+    def start_model_trainer(self
+    ) -> ModelTrainerArtifact:
+        try:
+            model_trainer = ModelTrainer(
+                model_trainer_config=self.model_trainer_config,
+            )
+            model_trainer_artifact = model_trainer.initiate_model_trainer()
+            return model_trainer_artifact
+
         except Exception as e:
             raise SignException(e, sys)
